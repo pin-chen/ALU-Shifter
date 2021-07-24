830d6db60e52116c423528633a2c46f0e7adc2d8
pinchen652 <hardymike6308@gmail.com>
2021-07-24 14:22:03 +0800

    NOR

--- CO_Lab2/ALU.v
+++ CO_Lab2/ALU.v
@@ -67,10 +67,6 @@ module ALU( result, zero, overflow, aluSrc1, aluSrc2, invertA, invertB, operatio
 	xor (Over, carry[32], carry[31]);
 	MUX_4to2 M4({1'b0, 1'b0, Over, 1'b0}, operation, overflow);
 	//Get zero
-	reg Zero;
-	always@(*)
-		if(result == 32'b0) Zero = 1'b1;
-		else Zero = 1'b0;
-	assign zero = Zero;
+	nor (zero, result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7], result[8], result[9], result[10], result[11], result[12], result[13], result[14], result[15], result[16], result[17], result[18], result[19], result[20], result[21], result[22], result[23], result[24], result[25], result[26], result[27], result[28], result[29], result[30], result[31]);
 	//
 endmodule
